81f7ff1ab98f7da2f22d82848b6404e54aa14192
Michael Fisher <mfisher.au@gmail.com>
2018-04-10 22:46:44 +0000

    Something funky happened

--- packages/ozp-sample-app/src/AppRouter.tsx
+++ packages/ozp-sample-app/src/AppRouter.tsx
@@ -1,10 +1,8 @@
 import * as React from "react";
 import { Router } from "@twii/router/lib/Router";
 import { exactPath } from "@twii/router/lib/Routers";
-import { createSampleRouter } from "@twii/sample-base/lib/sampleRouter";
 
 const r = new Router();
-r.use(createSampleRouter());
 
 r.use((req, next) => {
     if(req.path === "/" || req.path === "/index" || req.path === "/dashboards") {
