bba354cd15cb78c5b8c7f568f7512c0be20331fe
Michael Fisher <mfisher.au@gmail.com>
2019-05-06 11:00:46 +1000

    Moved to hooks.

--- packages/core-react/src/component/AppFrame.tsx
+++ packages/core-react/src/component/AppFrame.tsx
@@ -1,81 +1,58 @@
 import * as React from "react";
-import { IAppHostBaseProps } from "./IAppHostBaseProps";
+import { IEventTarget } from "@twii/core/lib/IEventEmitter";
 
-interface IAppFrame {
-    containerRef: HTMLDivElement;
-    frameRef: HTMLIFrameElement;
-}
-
-interface IAppFrameProps extends IAppHostBaseProps {
+interface IAppFrameProps {
     src?: string;
     className?: string;
-    componentRef?: (frame : IAppFrame) => void;
+    host?: IEventTarget;
 }
 
-class AppFrame extends React.Component<IAppFrameProps, any> implements IAppFrame {
-    private _frameRef : HTMLIFrameElement;
-    private _containerRef : HTMLDivElement;
-    private _onResize = () => {
-        if(this._containerRef && this._frameRef) {
-            const bounds = this._containerRef.getBoundingClientRect();
-            this._frameRef.width = String(bounds.width);
-            this._frameRef.height = String(bounds.height);
-        }
-    }
-    get frameRef() {
-        return this._frameRef;
-    }
-    get containerRef() {
-        return this._containerRef;
-    }
-    componentDidMount() {
-        const host = this.props.host;
+const AppFrame = (props : IAppFrameProps) => {
+    const containerRef = React.useRef<HTMLDivElement>();
+    const frameRef = React.useRef<HTMLIFrameElement>();
+    React.useEffect(() => {
+        const onResize = () => {
+            if(containerRef.current && frameRef.current) {
+                const bounds = containerRef.current.getBoundingClientRect();
+                frameRef.current.width = String(bounds.width);
+                frameRef.current.height = String(bounds.height);
+            }
+        };
+
+        const host = props.host;
         if(host) {
-            host.addEventListener("resize", this._onResize);
-            this._onResize();
+            host.addEventListener("resize", onResize);
+            onResize();
+            return () => {
+                host.removeEventListener("resize", onResize);
+                if(frameRef.current) {
+                    frameRef.current.src = "about:blank";
+                }
+            };
         }
-    }
-    componentWillUnmount() {
-        if(this.props.host) {
-            this.props.host.removeEventListener("resize", this._onResize);
-        }
-        if(this._frameRef) {
-            this._frameRef.src = "about:blank";
-        }
-    }
-    private _onContainerRef = (ref : HTMLDivElement) => {
-        this._containerRef = ref;
-        if(this.props.componentRef) {
-            this.props.componentRef(this._containerRef ? this : undefined);
-        }
-        this._onResize();
-    }
-    private _onFrameRef = (ref : HTMLIFrameElement) => {
-        this._frameRef = ref;
-    }
-    render() {
-        return (
-            <div className={this.props.className}
-                 style={
-                     { overflow: "hidden",
-                        position: "absolute",
-                        top: 0,
-                        right: 0,
-                        bottom: 0,
-                        left: 0
-                    }
-                 } ref={this._onContainerRef}>
-                <iframe style={
-                    {
-                        position: "absolute",
-                        top: 0,
-                        left: 0,
-                        border: "none"
-                    }
-                } ref={this._onFrameRef} src={this.props.src} />
-            </div>
-        );
-    }
+    });
+    return (
+        <div className={props.className}
+             style={
+                 { overflow: "hidden",
+                    position: "absolute",
+                    top: 0,
+                    right: 0,
+                    bottom: 0,
+                    left: 0
+                }
+             } ref={containerRef}>
+            <iframe style={
+                {
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    border: "none"
+                }
+            } ref={frameRef} src={props.src} />
+        </div>
+    );
 };
 
-export { IAppFrameProps, IAppFrame, AppFrame }
+
+export { IAppFrameProps, AppFrame }
